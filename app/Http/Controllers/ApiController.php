286397d6f219330f47c25681c19e2596e061c474
dblumenau <dblumenau@gmail.com>
2018-02-05 12:55:48 +0200

    no time

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Api;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Api $api)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Api $api)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Api $api)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Api $api)
+    {
+        //
+    }
+	
+	public function acceptData(Request $request)
+	{
+		dd($request);
+    }
+}
